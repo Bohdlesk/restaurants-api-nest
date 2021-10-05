d4c162b4763616f46ed491f261da1cde2e962ab1
Bohdlesk <bohdanleskovets@gmail.com>
2021-10-05 23:24:05 +0300

    Cors added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  process.on('unhandledRejection', (reason, p) => {
+    console.error(JSON.stringify({ p, reason }));
+  });
+
+  process.on('uncaughtException', (error) =>
+    console.error(JSON.stringify(error)),
+  );
+
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cors);
 
   const port = process.env.port || 3000;
   await app.listen(port);
