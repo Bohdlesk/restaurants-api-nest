81ccdc65f7baf040ee88876369a53e930aa3c7a2
Bohdlesk <bohdanleskovets@gmail.com>
2021-11-11 10:12:29 +0200

    Entities moved

--- src/modules/reviews/reviews.module.ts
+++ src/modules/reviews/reviews.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Review } from './entities/review.entity';
+import { Review } from '../../entities/review.entity';
+import { RestaurantsModule } from '../restaurants/restaurants.module';
+import { Restaurant } from '../../entities/restaurant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review])],
+  imports: [TypeOrmModule.forFeature([Review, Restaurant]), RestaurantsModule],
   exports: [TypeOrmModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
